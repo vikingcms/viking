3a2b5677d08e1180da560649b75e9cc6f5295735
Tony Lea <tony@tonys-mbp-2.home>
2019-12-24 12:01:07 -0800

    Adding the posts page

--- src/cli/dashboard.js
+++ src/cli/dashboard.js
@@ -2,55 +2,63 @@
 const http = require('http')
 const getPort = require('get-port')
 const open = require('open')
-var express = require('express')
-var app = express()
+let express = require('express')
+let app = express()
 let pathToPackage = require("global-modules-path").getPath("vikingcms");
-
 const curDir = process.cwd();
+const fs = require('fs');
+const postsFolder = curDir + '/content/posts/json/';
+const debug = true;
 
 async function getRandomPort (preferredPort = 8000) {
     const port = await getPort({ port: preferredPort });
     return port;
 }
 
-module.exports = {
+let dashboard = module.exports = {
     launch: function(){
-        console.log('Welcome to VikingCMS');
 
         getRandomPort(8080).then( function(port){
-            
-            // const requestHandler = (request, response) => {
-            //     console.log(request.url)
-            //     response.end('Hello Node.js Server!')
-            // }
 
             app.set('view engine', 'ejs');
-            app.get('/', (req, res) => res.render(pathToPackage + '/src/dashboard/index') );
             app.use('/assets', express.static(pathToPackage + '/src/dashboard/assets'));
-            app.get('/posts', function(req, res){
-                res.send('rad');
-            });
+            
+            dashboard.generateRoutes();
 
-            app.get('/post/:post/', function(req, res){
-                res.send('rad ' + req.params.post);
-            });
-
-            app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+            app.listen(port, () => console.log(`VikingCMS dashboard running on port ${port}!`))
             
             open('http://localhost:' + port);
 
-            // const server = http.createServer(requestHandler)
-            
-            // server.listen(port, (err) => {
-            //     if (err) {
-            //     return console.log('something bad happened', err)
-            //     }
-            
-            //     console.log(`server is listening on ${port}`)
-
-            //     open('http://localhost:' + port);
-            // });
         });
 
+    },
+
+    generateRoutes: function(){
+        
+        app.get('/', (req, res) => res.render(pathToPackage + '/src/dashboard/index', { request: req }) );
+        
+        app.get('/posts', function(req, res){
+            dashboard.getPosts(function(posts){
+                res.render(pathToPackage + '/src/dashboard/posts', { request: req, posts: posts, debug: debug });
+            });
+        });
+
+        app.get('/post/:post', function(req, res){
+            //res.send('rad ' + req.params.post, { request: req });
+            res.send('rad ' + req.params.post);
+        });
+
+    }, 
+
+    getPosts: function(_callback){
+        let posts = [];
+        fs.readdir(postsFolder, (err, files) => {
+            files.forEach(file => {
+                //console.log(file);
+                let filename = file.replace('.json', '');
+                posts.push( JSON.parse( fs.readFileSync( postsFolder + file ) ) );
+            });
+            _callback(posts);
+        });
     }
 }
